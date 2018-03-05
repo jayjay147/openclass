48bc8d4e58f1cbfee95a459f335b06e298e905f6
jay147 <jayjayr147@gmail.com>
2018-03-05 15:27:12 +0100

    modification bissextile.py - import mod os - utilisation os.system pour mettre pause à la fin de l'execution du programme

--- python/bissextile.py
+++ python/bissextile.py
@@ -1,5 +1,9 @@
 # programme déterminant si une année est bissextile ou non
 
+
+import os # importe le mod os var et fonctions utiles pr dialoguer avc sys exploit.
+
+
 annee = input("Merci de saisir une année : ") # Saisie d'une année
 annee = int(annee) # variable année en type entier et non chaines de caractères
 
@@ -17,4 +21,6 @@ if bissextile:
 else:
 	print("l\'année n'est pas bissextile")
 
-exit()
+os.system("pause") # dans le module os on utilise la fonction system
+
+# la fonction system demande à windows de ne pas fermer le programme une fois executé
