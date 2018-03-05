7458b1a34d5230e7c5b87821fae0c2e281d8aa93
jay147 <jayjayr147@gmail.com>
2018-03-05 14:42:37 +0100

    modification fonctions.py

--- python/fonctions.py
+++ python/fonctions.py
@@ -2,12 +2,11 @@
 
 # calculer la table de multiplication
 
-nb = input("Merci de saisir par un chiffre, la table de multiplication que vous voulez calculer : ")
-# formulaire 
-nb = int(nb) # variable en nombre entier
 
-i = 0 # variable compteur qui incrémentera dans le calcul
 
-while i < 10: # tant que i est strictement inférieur à 10
-	print(i+1, "x", nb, "=", (i+1) * nb)
-	i += 1 # on incrémente i de + 1 à chaque tour
+def table(nb): # Création fonction
+	i = 0 # variable compteur incrémentation dans le calcul
+	#boucle
+	while i < 10:
+		print(i + 1, "x", nb, "=", (i + 1) * nb)
+		i += 1 # on incremente de 1 à chaque tour de la boucle.
