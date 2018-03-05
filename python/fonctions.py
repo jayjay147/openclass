241f8648d8b9e5e993e5c4926a75403eebb3e134
jay147 <jayjayr147@gmail.com>
2018-03-05 14:10:40 +0100

    Création fichier fonctions.py

---
+++ python/fonctions.py
@@ -0,0 +1,13 @@
+# Apprentissage fonctions - reprise exercice sur la table de multiplication
+
+# calculer la table de multiplication
+
+nb = input("Merci de saisir par un chiffre, la table de multiplication que vous voulez calculer : ")
+# formulaire 
+nb = int(nb) # variable en nombre entier
+
+i = 0 # variable compteur qui incrémentera dans le calcul
+
+while i < 10: # tant que i est strictement inférieur à 10
+	print(i+1, "x", nb, "=", (i+1) * nb)
+	i += 1 # on incrémente i de + 1 à chaque tour
